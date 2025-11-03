3eaf50763cedd5f67a367fb3243058cabbcc8cc9
sunithadhanam <sunitha.dhanamjayam@spark.co.nz>
2025-11-03 03:38:23 +0000

    modified the length of order column

---
+++ tasty_bytes_setup.sql
@@ -0,0 +1,5 @@
+SHOW DBT WORKSPACES;--will not run until you connect to dbt deploy project
+SHOW EXTERNAL ACCESS INTEGRATIONS;
+SHOW NETWORK RULES;
+SHOW TABLES IN DATABASE tasty_bytes_dbt_db;
+SHOW VIEWS IN DATABASE tasty_bytes_dbt_db;
